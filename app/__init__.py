5effe55d86d6ef7d90dd5e539a748d0b4fd7538b
Alessandro <alessandro.cifani@gmail.com>
2017-05-09 16:28:59 +0200

    defined db models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from config import app_config
 
 db = SQLAlchemy()
+lm = LoginManager()
 
 
 def create_app(config_name):
@@ -12,8 +15,20 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    # test route -- to remove
     @app.route('/')
     def hello_world():
         return 'Hello, world!'
+
+    # login manager setup
+    lm.init_app(app)
+    lm.login_message = "You must be logged in to access this page"
+    lm.login_view = "auth.login"
+
+    # migrate setup
+    migrate = Migrate(app, db)
+
+
+    from app import models
 
     return app
---
+++ app/models.py
@@ -0,0 +1,53 @@
+from flask_login import UserMixin
+
+# imports application and login manager
+from app import db, lm
+
+"""
+Create helper table for user and game relationship
+"""
+usergames = db.Table('usergames', db.Model.metadata,
+                db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                db.Column('game_isr', db.Integer, db.ForeignKey('games.id')))
+
+
+class User(UserMixin, db.Model):
+    """
+    Defines user table
+    """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(64), nullable=True, unique=True)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Game(db.Model):
+    """
+    Defines game table
+    """
+
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    # so that we can use game.users and user.games to get list of
+    # matching items
+    users = db.relationship('User',
+                            secondary=usergames,
+                            backref='games',
+                            lazy='dynamic')
+
+    def __repr__(self):
+        return '<Game: {}>'.format(self.name)
